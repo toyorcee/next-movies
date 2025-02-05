1a3f683eef93d2183a665f72cea7666adf4472a5
toyorcee <oluwatoyosiolaniyan@gmail.com>
2025-02-05 22:52:48 +0100

    Fetch and display movies results and Card components

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import ThemeCom from "@/components/ThemeCom";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,6 +29,7 @@ export default function RootLayout({ children }) {
         >
           <ThemeCom>
             <Header />
+            <Navbar />
             {children}
           </ThemeCom>
         </body>
